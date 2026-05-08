de12d14568758b172f20f7f2a88b5ee8497fd8be
Dimitris Arvanitis <arvanitisd28@gmail.com>
2026-05-08 12:56:38 +0300

    Implemented get_all_calls function

--- src/services/call_service.py
+++ src/services/call_service.py
@@ -1,5 +1,12 @@
 def get_all_calls():
     # return all non-archived calls from the in-memory store
+    filtered_calls = []
+    for call in seed_data:
+        if not call["is_archived"]:
+            filtered_calls.append()
+
+    return filtered_calls
+
     pass
 
 def get_call_by_id(call_id):
@@ -8,4 +15,8 @@ def get_call_by_id(call_id):
 
 def archive_call(call_id):
     # set is_archived to True, return the updated call, or None if not found
+    if "is_archived" == True:
+        return call_id
+    else:
+        return None
     pass
